71e20498f8f0d044753e5a76bfb568c2343ea1bd
hslbi <hslbi@hanmail.net>
2022-02-12 23:06:44 +0900

    Initial commit

---
+++ src/test/java/net/jounsesang/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jounsesang.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
